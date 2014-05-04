87f9b5aff058b8d4062c2d5e3ca54e3435ba918a
Daniel Du <duchangyu@gmail.com>
2014-05-04 18:37:13 +0800

    document

--- DummyService/Controllers/LicenseController.cs
+++ DummyService/Controllers/LicenseController.cs
@@ -1,4 +1,23 @@
-﻿using DummyService.Models;
+﻿////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved 
+// Written by Daniel Du 2014 - ADN/Developer Technical Services
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted, 
+// provided that the above copyright notice appears in all copies and 
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting 
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC. 
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////////////////
+
+
+using DummyService.Models;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -59,9 +78,7 @@ namespace DummyService.Controllers
             req.Method = Method.GET;
             req.AddParameter("userid", userId);
             req.AddParameter("appid", appId);
-            #region Required for Autodesk Exchange staging server
-            req.AddCookie("RequestAccessCookie", "Aut0d3sk");
-            #endregion
+
 
             IRestResponse<EntitlementResult> resp= client.Execute<EntitlementResult>(req);
 
